c4d40c76ef4c811a9bc00fc14e5149f3f6d457ac
nash junior <nashjuniorjj@gmail.com>
2026-08-01 16:58:51 -0300

    RAZ-P0: trava registro do EPP de cofre no Boot

---
+++ plataforma/plataforma-infra/src/test/java/br/contabil/plataforma/infra/cofre/CofreSegredosEnvironmentPostProcessorRegistroBootTest.java
@@ -0,0 +1,33 @@
+package br.contabil.plataforma.infra.cofre;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+
+@SpringBootTest(
+        classes = CofreSegredosEnvironmentPostProcessorRegistroBootTest.AplicacaoMinima.class,
+        webEnvironment = SpringBootTest.WebEnvironment.NONE,
+        args = {
+                "--spring.datasource.password-ref=env:SIAFIC_F0_DB_RUNTIME_PASSWORD",
+                "--spring.flyway.password-ref=env:SIAFIC_F0_DB_MIGRATION_PASSWORD"
+        })
+class CofreSegredosEnvironmentPostProcessorRegistroBootTest {
+
+    @Autowired
+    private Environment environment;
+
+    @Test
+    void bootDescobreEnvironmentPostProcessorViaSpringFactories() {
+        assertThat(environment.getProperty("spring.datasource.password"))
+                .isEqualTo("nao-usado-dynamicpropertysource-sobrescreve");
+        assertThat(environment.getProperty("spring.flyway.password"))
+                .isEqualTo("nao-usado-dynamicpropertysource-sobrescreve");
+    }
+
+    @SpringBootConfiguration
+    static class AplicacaoMinima {}
+}
